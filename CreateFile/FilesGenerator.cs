8bd5cb740ba4046867334c2315c59042f3dad374
Heiner <heiner.suter@zuehlke.com>
2022-03-22 12:30:43 +0100

    Add project files.

---
+++ CreateFile/FilesGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace CreateFile;
+
+internal static class FilesGenerator
+{
+    public static void GenerateFiles(Options options)
+    {
+        for (var i = 0; i < options.Count; i++)
+        {
+            var filename = GenerateFilename(options, i);
+            var size = options.Size * Math.Pow(1024, (int)options.SizeUnit);
+            GenerateFile(filename, (long)size);
+            Console.WriteLine($"File '{filename}' generated with size '{options.Size} {options.SizeUnit}'.");
+        }
+    }
+
+    private static string GenerateFilename(Options options, int index)
+    {
+        if (options.Count > 1)
+        {
+            return $"{Path.GetFileNameWithoutExtension(options.NamePattern)}-{index}{Path.GetExtension(options.NamePattern)}";
+        }
+
+        return options.NamePattern;
+    }
+
+    private static void GenerateFile(string filename, long size)
+    {
+        using var fs = new FileStream(filename, FileMode.Create);
+        fs.Seek(size, SeekOrigin.Begin);
+        fs.WriteByte(0);
+    }
+}
---
+++ CreateFile/Program.cs
@@ -0,0 +1,7 @@
+﻿using CommandLine;
+using CreateFile;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(FilesGenerator.GenerateFiles);
+
+Console.Read();
